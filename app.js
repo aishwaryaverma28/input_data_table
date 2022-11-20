53fdb49baa53908a336c4710bfb7b1724b7cbe81
Aishwarya <aishwarya.verma28@gmail.com>
2022-11-20 10:43:39 +0530

    delete table rows added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //======================================== getting data from form======================================================
 document.querySelector("form").addEventListener("submit",inputFields);
- let studentName="",studentEmail="",studentPhNo="";
+let studentName="",studentEmail="",studentPhNo="";
 function inputFields(){
 event.preventDefault();
 studentName=document.querySelector("#name").value;
@@ -20,9 +20,22 @@ td2.innerText=studentEmail;
 let td3= document.createElement("td");
 td3.innerText=studentPhNo;
 let btn=document.createElement("button");
-btn.setAttribute("id","delete");
+btn.setAttribute("class","deleteBtn");
 btn.innerText="delete";
 tr.append(td1,td2,td3,btn);
 document.querySelector("tbody").append(tr);
 }
+}
+studentEmail.value="";
+studentPhNo.value="";
+
+//================================================deleting table row================================================================
+const tableEL=document.querySelector("table");
+tableEL.addEventListener("click",deleteRow);
+
+function deleteRow(e){
+  if(!(e.target.classList.contains("deleteBtn")))
+    return;
+    const btn=e.target;
+    btn.parentElement.remove();
 }
